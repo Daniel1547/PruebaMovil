f60c97a2f8b4923e264500d6f1363d0df311c573
daniel y Monse <daniel.tynig@gmail.com>
2022-11-17 11:01:51 -0600

    Primer commit

---
+++ PruebaMovil/PruebaMovil/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PruebaMovil.ViewModels;
+
+namespace PruebaMovil.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PruebaMovil/PruebaMovil/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PruebaMovil.Models;
+using PruebaMovil.ViewModels;
+
+namespace PruebaMovil.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
